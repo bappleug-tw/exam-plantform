32ae42b45a3d80962b1517048488f30c1edb414c
RayMin <rmin@thoughtworks.com>
2020-04-12 16:42:37 +0800

    test throw InvalidBlankQuizIdException when create paper with invalid quiz id

--- src/test/java/demo/domain/PaperTest.java
+++ src/test/java/demo/domain/PaperTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PaperTest {
 
@@ -17,5 +18,15 @@ public class PaperTest {
         Paper paper = Paper.create(blankQuizIds);
         assertThat(paper).isNotNull();
         assertThat(paper.getBlankQuizIds()).isEqualTo(blankQuizIds);
+    }
+
+    @Test
+    void should_return_paper_when_create_given_invalid_quiz_ids() {
+        List<String> blankQuizIds = new ArrayList<>();
+        blankQuizIds.add("invalid blank quiz 1");
+        blankQuizIds.add("invalid blank quiz 2");
+        //noinspection ResultOfMethodCallIgnored
+        assertThatThrownBy(() -> Paper.create(blankQuizIds))
+                .isInstanceOf(InvalidBlankQuizIdException.class);
     }
 }
